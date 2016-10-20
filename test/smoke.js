a52d162ecb7633d0ff18fe505fe01c524435cb5b
Noam Kfir <noam@kfir.cc>
2016-10-20 08:54:37 +0300

    Fixed sinon package reference.

--- test/smoke.js
+++ test/smoke.js
@@ -1,12 +1,24 @@
 import { expect } from 'chai';
+import { spy } from 'sinon';
 
 describe('smoke test', () => {
 
-    it('works!', () => {
+    it('Chai works!', () => {
 
         const value = 'You can test!';
 
         expect(value).to.be.a('string');
+
+    });
+
+    it('Sinon works!', () => {
+
+        const callback = spy();
+
+        const foo = fn => fn();
+        foo(callback);
+
+        expect(callback.called).to.be.true;
 
     });
 
