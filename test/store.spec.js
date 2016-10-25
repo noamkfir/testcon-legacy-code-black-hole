91162c5bd42753ca462cb0d60085607e1a655d18
Noam Kfir <noam@kfir.cc>
2016-10-26 01:31:45 +0300

    Added tests for unimplemented promise behavior.

--- test/store.spec.js
+++ test/store.spec.js
@@ -33,6 +33,35 @@ describe('Store', () => {
 
         });
 
+        it('should pass an empty array to the promise callback', (done) => {
+
+            const storageService = new StorageService('store-name');
+            const store = new Store(storageService);
+
+            store.find({})
+                .then((items) => {
+                    expect(items).to.have.length(0);
+                    done();
+                });
+
+        });
+
+        it('should pass a non-empty array to the promise callback', (done) => {
+
+            const storageService = new StorageService('store-name');
+            storageService.setData([{
+                title: 'sample item'
+            }]);
+            const store = new Store(storageService);
+
+            store.find({})
+                .then((items) => {
+                    expect(items).to.have.length(1);
+                    done();
+                });
+
+        });
+
     });
 
 });
