c93b8910033406a4ba6c8ef1e814f978066d7134
Noam Kfir <noam@kfir.cc>
2016-10-25 23:27:04 +0300

    Created a failing test to preserve the existing callback behavior of the Store#find method.

---
+++ test/store.spec.js
@@ -0,0 +1,21 @@
+import Store from "../src/store";
+import { expect } from 'chai';
+
+describe('Store', () => {
+
+    describe('find', () => {
+
+        it('should pass an empty array to the callback', (done) => {
+
+            const store = new Store('store-name');
+
+            store.find({}, (items) => {
+                expect(items).to.have.length(1);
+                done();
+            });
+
+        });
+
+    });
+
+});
